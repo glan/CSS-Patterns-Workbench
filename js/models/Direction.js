493a6e41da15fcf95a92cee6b02825ef0bd0c453
Glan Thomas <github@glanthomas.com>
2012-03-18 15:08:43 -0600

    Order of direction keywords agnostic

--- js/models/Direction.js
+++ js/models/Direction.js
@@ -6,6 +6,16 @@ define('models/Direction', function () {
     'use strict';
 
     function Direction(value) {
+        if (value === 'left top') {
+            value = 'top left';
+        } else if (value === 'right top') {
+            value = 'right left';
+        } else if (value === 'left bottom') {
+            value = 'bottom left';
+        } else if (value === 'right bottom') {
+            value = 'bottom right';
+        }
+
         this.value = (typeof value !== 'undefined') ? value : '90deg';
     }
 
